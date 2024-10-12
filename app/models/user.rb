32cd197bc566fdf156e7db7060952e5efe1db2b4
Varsik Orchanian <varsenik.ava@gmail.com>
2024-10-12 19:41:12 +0200

    Fix signup, remove unused address from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true, length: { maximum: 60 }
   validates :password, presence: true, length: { maximum: 20 }
-  validates :address, presence: true, length: { maximum: 255 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
