889ce5dc5e7c65f0e9c0aaf30108627fbc407f3b
Lise Weingart <lise.aline@gmail.com>
2024-10-16 18:14:56 +0200

    ajout de la clé api

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   #has_many :reviews
 
   validates :email, presence: true, uniqueness: true, length: { maximum: 60 }
-  validates :password, presence: true, length: { maximum: 20 }
+  # validates :password, presence: true, length: { maximum: 20 } #LW: je désactive car Devise s'en occupe déjà et cela peut donc créer un conflit avec Devise
   validates :address, allow_nil: true, length: { maximum: 255 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_15_191914) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "description"
     t.string "author"
     t.string "isbn"
     t.integer "price"
@@ -53,6 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_15_191914) do
     t.bigint "seller_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["seller_id"], name: "index_books_on_seller_id"
   end
 
