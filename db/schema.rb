30fa1987502933d47dee6a479f4271e62396b794
Velluz Roseline <roseline.velluz@gmail.com>
2024-10-17 17:03:13 +0200

    ajout des cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_16_191914) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_16_162129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,7 +64,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_16_191914) do
     t.bigint "seller_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "description"
+    t.string "description"
     t.index ["seller_id"], name: "index_books_on_seller_id"
   end
 
