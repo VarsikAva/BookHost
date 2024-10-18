7be9c84f7d1017431d6243c476196aa858329127
Varsik Orchanian <varsenik.ava@gmail.com>
2024-10-18 18:12:27 +0200

    Start message system

---
+++ db/migrate/20241017223150_create_initial_schema.rb
@@ -0,0 +1,103 @@
+class CreateInitialSchema < ActiveRecord::Migration[7.1]
+  def change
+    create_table "active_storage_attachments", force: :cascade do |t|
+      t.string "name", null: false
+      t.string "record_type", null: false
+      t.bigint "record_id", null: false
+      t.bigint "blob_id", null: false
+      t.datetime "created_at", null: false
+      t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+      t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+    end
+
+    create_table "active_storage_blobs", force: :cascade do |t|
+      t.string "key", null: false
+      t.string "filename", null: false
+      t.string "content_type"
+      t.text "metadata"
+      t.string "service_name", null: false
+      t.bigint "byte_size", null: false
+      t.string "checksum"
+      t.datetime "created_at", null: false
+      t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+    end
+
+    create_table "active_storage_variant_records", force: :cascade do |t|
+      t.bigint "blob_id", null: false
+      t.string "variation_digest", null: false
+      t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+    end
+
+    create_table "bookings", force: :cascade do |t|
+      t.bigint "user_id", null: false
+      t.bigint "book_id", null: false
+      t.date "start_date"
+      t.date "end_date"
+      t.string "status"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["book_id"], name: "index_bookings_on_book_id"
+      t.index ["user_id"], name: "index_bookings_on_user_id"
+    end
+
+    create_table "books", force: :cascade do |t|
+      t.string "title"
+      t.string "author"
+      t.string "isbn"
+      t.integer "price"
+      t.integer "stock_quantity"
+      t.string "image_url"
+      t.bigint "seller_id", null: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.text "description"
+      t.index ["seller_id"], name: "index_books_on_seller_id"
+    end
+
+    create_table "orders", force: :cascade do |t|
+      t.bigint "user_id", null: false
+      t.bigint "book_id", null: false
+      t.integer "quantity"
+      t.float "total_price"
+      t.date "start_date"
+      t.date "end_date"
+      t.string "shipping_adress"
+      t.string "order_status"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["book_id"], name: "index_orders_on_book_id"
+      t.index ["user_id"], name: "index_orders_on_user_id"
+    end
+
+    create_table "users", force: :cascade do |t|
+      t.string "email", default: "", null: false
+      t.string "encrypted_password", default: "", null: false
+      t.string "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.string "first_name"
+      t.string "last_name"
+      t.string "address"
+      t.index ["email"], name: "index_users_on_email", unique: true
+      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    end
+
+    create_table "messages", force: :cascade do |t|
+      t.bigint "sender_id"
+      t.bigint "receiver_id"
+      t.text "content"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+
+    add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+    add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+    add_foreign_key "bookings", "books"
+    add_foreign_key "bookings", "users"
+    add_foreign_key "books", "users", column: "seller_id"
+    add_foreign_key "orders", "books"
+    add_foreign_key "orders", "users"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_16_191914) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_17_223150) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_16_191914) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.index ["seller_id"], name: "index_books_on_seller_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "sender_id"
+    t.bigint "receiver_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
