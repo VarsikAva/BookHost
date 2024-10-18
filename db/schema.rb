f262f550067a56b83d0f65933774d9b9f2f1b31c
Velluz Roseline <roseline.velluz@gmail.com>
2024-10-18 20:39:18 +0200

    nouvelle carte show

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_17_223150) do
     t.bigint "seller_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "description"
+    t.text "description"
     t.index ["seller_id"], name: "index_books_on_seller_id"
   end
 
